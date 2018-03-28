01c41629f1403fc5b564da186b585fc6447fc2f1
Anthony Jackman <energeticpixels@gmail.com>
2018-03-27 19:54:17 -0500

    replace dummy data with api data

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,22 +1,13 @@
 <template>
     <div id="movie-list">
-        <div v-for="movie in filteredMovies" class="movie">{{ movie.title }}</div>
+        <div v-for="movie in filteredMovies" class="movie">{{ movie.movie.Title }}</div>
     </div>
 </template>
 
 <script>
     import genres from '../util/genres';
     export default {
-        data: function() {
-            return {
-                movies: [
-                    {title: "kill bill", genre: genres.CRIME},
-                    {title: "home alone", genre: genres.COMEDY},
-                    {title: "austin powers", genre: genres.COMEDY}
-                ]
-            };
-        },
-        props: [ 'genre', 'time' ],
+        props: [ 'genre', 'time', 'movies' ],
         methods: {
             moviePassesGenreFilter(movie) {
                 if (!this.genre.length) {
