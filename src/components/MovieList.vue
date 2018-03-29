644aa8c267c224f1a01821c25925822c22b938ea
Anthony Jackman <energeticpixels@gmail.com>
2018-03-28 22:01:37 -0500

    enhance no results message

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -5,7 +5,7 @@
             </movie-item>
         </div>
         <div v-else-if='movies.length' class='no-results'>
-            No results. 
+            {{ noResults }} 
         </div>
         <div v-else class='no-results'>
             Loading
@@ -51,6 +51,11 @@
                 return this.movies
                     .filter(this.moviePassesGenreFilter)
                     .filter(movie => movie.sessions.find(this.sessionPassesTimeFilter));
+            },
+            noResults() {
+                let times = this.time.join(', ');
+                let genres = this.genre.join(', ');
+                return `No results for ${times}${times.length && genres.length ? ', ' : ''}${genres}.`;
             }
         },
         components: {
