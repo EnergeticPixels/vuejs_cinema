0f0200a35742987c6d0097de3891a8640742a069
Anthony Jackman <energeticpixels@gmail.com>
2018-03-30 21:05:02 -0500

    add classes and evtHandlers to tooltip

--- src/main.js
+++ src/main.js
@@ -42,12 +42,39 @@ new Vue({
 
 });
 
+import { addClass, removeClass } from './util/helpers';
+
+let mouseOverHandler = function(event) {
+    let span = event.target.parentNode.getElementsByTagName('SPAN')[0];
+    addClass(span, 'tooltip-show');
+};
+
+let mouseOutHandler = function(event) {
+    let span = event.target.parentNode.getElementsByTagName('SPAN')[0];
+    removeClass(span, 'tooltip-show');
+};
+
 Vue.directive('tooltip', {
     bind(el, bindings) {
         //console.log(el);
         let span = document.createElement('SPAN');
         let text = document.createTextNode('Seats available: 200');
+        // adding classes to this can have cross-browser issues
         span.appendChild(text);
+        addClass(span, 'tooltip');
         el.appendChild(span);
+        let div = el.getElementsByTagName('DIV')[0];
+        div.addEventListener('mouseover', mouseOverHandler);
+        div.addEventListener('mouseout', mouseOutHandler);
+        // 'touch...' is for mobile people
+        div.addEventListener('touchstart', mouseOverHandler);
+        div.addEventListener('touchend', mouseOutHandler);
+    },
+    unbind(el) {
+        let div = el.getElementsByTagName('DIV')[0];
+        div.removeEventListener('mouseover', mouseOverHandler);
+        div.removeEventListener('mouseout', mouseOutHandler);
+        div.removeEventListener('touchstart', mouseOverHandler);
+        div.removeEventListener('touchend', mouseOutHandler);
     }
 });
