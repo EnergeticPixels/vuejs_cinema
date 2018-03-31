3173705afd414218c236242e87950843f6603fb6
Anthony Jackman <energeticpixels@gmail.com>
2018-03-30 19:51:19 -0500

    adding days to daySelect comp

--- src/components/DaySelect.vue
+++ src/components/DaySelect.vue
@@ -1,13 +1,28 @@
 <template>
     <div id='day-select'>
-        Hellow World
+        <ul class='days'>
+            <li class='day' v-for='day in days'>{{ formatDay(day) }}</li>
+        </ul>
 
     </div>
 </template>
 
 <script>
     export default {
-        
+        data() {
+            return {
+                days: [0, 1, 2, 3, 4, 5, 6].map(num => this.$moment().add(num, 'days'))
+            }
+        },
+        methods: {
+            formatDay(raw) {
+                if (raw.isSame(this.$moment(), 'day')) {
+                    return 'Today';
+                } else {
+                    return raw.format('ddd DD/MM');
+                }
+            }
+        }
     }
 </script>
 
